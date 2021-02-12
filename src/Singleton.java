f2c26b9977e428f40fdab42c4490ec11a536333b
kanishka424 <kanishkamalpana24@gmail.com>
2021-02-12 10:32:15 +0530

    Simple lazy initalization Singleton code

---
+++ src/Singleton.java
@@ -0,0 +1,15 @@
+public class Singleton {
+    private static Singleton firstInstance=null;
+
+    private Singleton(){
+
+    }
+
+    public static Singleton getInstance(){
+        if (firstInstance==null){
+            firstInstance=new Singleton();
+        }
+
+        return firstInstance;
+    }
+}
